b3b81554ee7a19f95d551b9656e99218239ea223
Otabekfrontend <otabekmadaminov1022@gmail.com>
2024-04-27 01:45:53 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,144 @@
+"use strict";
+
+// masala 1
+// function sum(x, y) {
+//     console.log(x + y);
+// }
+
+// sum(-3, -6);
+
+// masala 2
+
+// function toSekund(minutes) {
+//     console.log(60 * minutes);
+// }
+
+// toSekund(1);
+
+// masala 3
+
+// function nextInteger(int) {
+//     console.log(int + 1);
+// }
+// nextInteger(-3);
+
+// masala 4
+// function uchburchkYuzi(asos, balandlik) {
+//     console.log(`${(asos * balandlik) / 2}`);
+// }
+// uchburchkYuzi(10, 5);
+
+// masala 5
+// function ageToDays(age) {
+//     console.log(`${age * 365} kun`);
+// }
+// ageToDays(65);
+
+// masala 6
+// function cub(int) {
+//     console.log(`${int ** 3}`);
+// }
+// cub(10);
+
+// masala 7
+// function firstElement(arr) {
+//     console.log(arr[0]);
+// }
+// firstElement([1, 2, 3]);
+// firstElement([80, 10, 5]);
+// firstElement([-500, 0, 50]);
+
+// masala 8
+// function power(x, y) {
+//     console.log(`${x * y}`);
+// }
+// power(230, 10);
+// power(110, 3);
+// power(480, 20);
+
+// masala 9
+// function hourToSekund(hour) {
+//     console.log(`${hour * 3600} sekund`);
+// }
+// hourToSekund(2);
+// hourToSekund(10);
+// hourToSekund(24);
+
+// masala 10
+// function uchinchiTomon(tomon1, tomon2) {
+//     console.log(`${tomon1 + tomon2 - 1}`);
+// }
+// uchinchiTomon(8, 10);
+// uchinchiTomon(5, 7);
+// uchinchiTomon(9, 2);
+
+// masala 11
+// function qoldiq(son1, son2) {
+//     console.log(`${son1 % son2}`);
+// }
+// qoldiq(1, 3);
+// qoldiq(3, 4);
+// qoldiq(-9, 45);
+// qoldiq(5, 5);
+
+// masala 12
+// function tortburchakYuzasi(boyi, eni) {
+// console.log(`${boyi * eni}`);
+// }
+// let tor = +prompt("To'rtburchakning bo'yi va enini uzunliklarini kiriting");
+// let tor2 = +prompt("To'rtburchakning bo'yi va enini uzunliklarini kiriting");
+// tortburchakYuzasi(tor, tor2);
+// tortburchakYuzasi(20, 10);
+// tortburchakYuzasi(2, 9);
+
+// masala 13
+
+// masala 14
+// function kvadrat(x) {
+//     console.log(`${x ** 2}`);
+// }
+// let kvadrat1 = +prompt("Istalgan son kiriting");
+// kvadrat(kvadrat1);
+
+// masala 15
+// function noldan(raqam) {
+//     if (raqam <= 0) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+// let zero = +prompt("Son kiriting");
+// noldan(zero);
+
+// masala 16
+// function ichkiBurchaklar(n) {
+//     console.log(`${(n - 2) * 180}`);
+// }
+// let gradus = +prompt("Burchaklar soninin kiriting");
+// ichkiBurchaklar(gradus);
+
+// masala 17
+// function ochkolar(x, y) {
+//     console.log(`${x * 2 + y * 3}`);
+// }
+// let result = +prompt("Ikki ochkoli to'plar soni");
+// let result1 = +prompt("Uch ochkoli to'plar soni");
+// ochkolar(result, result1);
+
+// masala 18
+// function addString(a) {
+//     console.log(a.concat("Edabit"));
+// }
+// let nameString = prompt("SO'Z kiriting");
+// addString(nameString);
+
+// masala 19
+function ikkitaSon(x, y) {
+    if (x + y < 100) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+let yigindi = +prompt("Son kiriting");
